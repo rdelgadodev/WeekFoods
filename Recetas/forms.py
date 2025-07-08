805da36f73be6c6e4137df390803574c4bcc60e0
RaulDM <raul.delgado@hotmail.com>
2025-07-08 23:30:36 +0200

    Últimos cambios responsive

--- Recetas/forms.py
+++ Recetas/forms.py
@@ -2,33 +2,28 @@ from django.forms import ModelForm, TextInput, NumberInput, Select, URLInput
 from django import forms
 from WeekFoodsApp.models import Recipe, Ingredient
 
-"""class CustomMMCF(forms.ModelMultipleChoiceField):
-    def label_from_instance(self, ingredient):
-        return ingredient.name"""
 
-class RecipeForms(ModelForm):  
+class RecipeForms(ModelForm):
 
-    ingredients = forms.ModelMultipleChoiceField(queryset = Ingredient.objects.all(),
-                                                 widget = forms.CheckboxSelectMultiple)
+    ingredients = forms.ModelMultipleChoiceField(queryset=Ingredient.objects.all(),
+                                                 widget=forms.CheckboxSelectMultiple)
 
     class Meta:
         model = Recipe
-        fields = ['name', 'elaboration', 'when_you_eat', 'calories', 'ingredients']
-        widgets = {'name': TextInput(attrs={'required':True}),
-                   'elaboration': URLInput(attrs={'required':True}),
-                   'when_you_eat': Select(attrs={'required':True}),
-                   'calories': NumberInput(attrs={'required':True})}
-        
+        fields = ['name', 'elaboration',
+                  'when_you_eat', 'calories', 'ingredients']
+        widgets = {'name': TextInput(attrs={'required': True, 'class': 'form-control'}),
+                   'elaboration': URLInput(attrs={'required': True, 'class': 'form-control'}),
+                   'when_you_eat': Select(attrs={'required': True}),
+                   'calories': NumberInput(attrs={'required': True, 'class': 'form-control'})}
 
-    
-        
 
 class IngredientForms(ModelForm):
     class Meta:
         model = Ingredient
         fields = ['name_ingredient', 'type_food', 'price']
         widgets = {
-            'name_ingredient': TextInput(attrs={'required':True}),
-            'type_food': Select(attrs={'required':True}),
-            'price': NumberInput(attrs={'required':True})
+            'name_ingredient': TextInput(attrs={'required': True, 'class': 'form-control'}),
+            'type_food': Select(attrs={'required': True, 'class': 'form-control'}),
+            'price': NumberInput(attrs={'required': True, 'class': 'form-control'})
         }
--- WeekFoodsApp/views.py
+++ WeekFoodsApp/views.py
@@ -2,42 +2,44 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 import random
 from .models import UserWeekfoods
+import requests
+
 
 def home(request):
     return render(request, 'WeekfoodsApp/home.html')
 
+
 def quienes_somos(request):
     return render(request, 'WeekfoodsApp/quienes_somos.html')
 
+
 def como_trabajamos(request):
     return render(request, 'WeekfoodsApp/como_trabajamos.html')
+
 
 @login_required
 def home_usuario(request):
 
     # En esta página inicial, vamos a sugerir al usuario 3 recetas al azar.
     # Obtenemos las recetas que el usuario tiene almacenadas en su cuenta.
-    
-    user_name=request.user # Consultamos que usuario esta ahora activo en la web
-    user_recipes = UserWeekfoods.objects.get(user=user_name) # almacenamos todos los datos de ese usuario
-    recipes = user_recipes.recipe.all() # Almacenamos todas las recetas de las que dispone.
-   
+
+    user_name = request.user  # Consultamos que usuario esta ahora activo en la web
+    # almacenamos todos los datos de ese usuario
+    user_recipes = UserWeekfoods.objects.get(user=user_name)
+    # Almacenamos todas las recetas de las que dispone y lo ponemos en formato lista para usar posteriormente random.sample.
+    recipes = list(user_recipes.recipe.all())
+
     # Creamos una lista donde almacenaremos 3 recetas que aleatoriamente serán escogidas.
     # Esa lista se pasará como contexto para su visualizacion.
     list_recipes_suggestion = list()
 
-    
-    
+    # Usamos condicional if para ir escogiendo las 3 recetas al azar.
+    # Si el lista de recetas es mayor o igual a 3, escogerá 3 al azar, si es menor, pondrá todas las que haya.
 
-    # Usamos bucle while para ir escogiendo recetas al azar.
-    # Mientras el total del listado no sea 3, seguirá buscando 
-    # y se usa condicional para que no aparezcan recetas repetidas.
-    while ((len(list_recipes_suggestion)) < 3):
-        random_recipe = random.choice(recipes)
-        if random_recipe not in list_recipes_suggestion:
-            list_recipes_suggestion.append(random_recipe)
-        
-   
+    if len(recipes) >= 3:
+        list_recipes_suggestion = random.sample(recipes, 3)
+
+    else:
+        list_recipes_suggestion = recipes
+
     return render(request, 'WeekFoodsApp/home_usuario.html', {'list_recipe': list_recipes_suggestion})
-
-
